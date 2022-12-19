4fba55882f0ac9f77a6c23c3a9f9cb342ffe1ab0
iamgauhar <iamgauhar@yahoo.com>
2022-12-19 23:40:00 +0530

    Frontednd ww

---
+++ scripts/cart.js
@@ -0,0 +1,286 @@
+// let data = [{
+//     name: "Arata Hair Styling and Shaping Gel with Organic Flaxseed Extracts",
+//     image: "https://images-static.nykaa.com/media/catalog/product/f/a/face-primer.jpg",
+//     price : 849
+// },
+// {
+//     name: "Parachute Advansed Coconut Water Hydrating Multipurpose Gel For Skin & Hair",
+//     image: "https://images-static.nykaa.com/media/catalog/product/8/9/8904245706623_02.jpg",
+//     price : 119
+// }
+// ]
+
+// import { navbar } from "../component/navbar.js"
+// let nav = document.getElementById("navbar")
+// nav.innerHTML = navbar()
+
+// import { FOOTER } from "../component/pagefooter.js"
+// let footerpage = document.querySelector("footer")
+// footerpage.innerHTML = FOOTER()
+
+let cart_div = document.getElementById("cart_div")
+let total_price = document.getElementById("Toatlrupee");
+let cal_total = document.getElementById("cal_total");
+let discount = document.getElementById("discount");
+let dis_inp = document.getElementById("dis_inp");
+
+let coupen_apply = document.getElementById("coupen_apply");
+coupen_apply.onclick = () => {
+    
+    if (dis_inp.value == "BB20"){
+        discount.innerText = `- ${Math.ceil(total * 20/100) }`
+        total_price.innerText = `₹ ${total - Math.ceil(total * 20 / 100) }`
+        alert("Hurry! Coupen applied succsessfully")
+    } else{
+        alert("Invalid Coupen!")
+    }
+
+}
+
+let cartData = async () => {
+        await fetch(`https://rich-gray-dolphin-hat.cyclic.app/cart` , {
+            headers : {
+                authorization : `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+        .then((res) => res.json())
+        .then((res) => {    
+            append(res)
+        })
+        .catch((err) => console.log(err))
+}
+cartData()
+
+function append(data) {
+    cart_div.innerHTML = null
+    data.forEach(function (prod) {
+        let el = prod.productDetails
+        // console.log(el.image[0]);
+        let cart_product = document.createElement("div");
+        cart_product.setAttribute("id", "cart_product");
+
+        let img_div = document.createElement("div")
+        img_div.setAttribute("id" , "img_div")
+        let image = document.createElement("img");
+        image.src = el.image[0];
+
+        let details = document.createElement("div");
+        details.setAttribute("id" , "details");
+
+        let title_div = document.createElement("div");
+        title_div.setAttribute("id" , "title_div")
+
+        let des = document.createElement("p");
+        des.setAttribute("class", "description");
+        des.innerText = el.name;
+
+        let para_div = document.createElement("div");
+        para_div.setAttribute("id" , "para_div")
+
+        let price = document.createElement("p");
+        price.innerText = `₹ ${el.price}`;
+        price.setAttribute("id", "item_price");
+        priceIn(el.price)
+
+        let qty_div = document.createElement("div");
+        qty_div.setAttribute("id" , "qty_div")
+            let qty_box = document.createElement("p");
+            qty_box.setAttribute("id" , "qtyVal")
+            qty_box.innerText = "Qty : 1"
+            // qty_box.type = Number;
+            let qtyAdd = document.createElement("button");
+            qtyAdd.innerText = "+";
+            qtyAdd.onclick = () => {
+                incQty(price , el.price, qty_box , qtyAdd, qtyLess)
+            }
+            let qtyLess = document.createElement("button");
+            qtyLess.innerText = "-";
+            qtyLess.onclick = () => {
+                decQty(price, el.price, qty_box, qtyAdd, qtyLess)
+            }
+
+        let icon = document.createElement("p");
+        icon.setAttribute("id", "del_icon")
+        icon.innerHTML = `<i class="fa fa-trash-o" style="font-size:16px"></i>`
+        icon.onclick = function () {
+            deleteProduct(prod)
+            priceDe(el.price)
+        }
+
+        qty_div.append(qty_box,qtyAdd,qtyLess)
+        img_div.append(image)
+        details.append(title_div , para_div)
+        title_div.append(des)
+        para_div.append(price,qty_div,icon)
+        cart_product.append(img_div, details)
+        cart_div.append(cart_product)
+    });
+}
+
+let total = 0
+
+function priceIn(el){
+    total = total + el;
+    total_price.innerText = `₹ ${total}`
+    cal_total.innerText = `₹ ${total}`
+}
+
+function priceDe(el) {
+    total = total - el;
+    total_price.innerText = `₹ ${total}`
+    cal_total.innerText = `₹ ${total}`
+}
+let qty = 1
+
+function incQty(price , pro_price, qty_box , qtyAdd , qtyLess){
+    if(qty == 4){
+        qtyAdd.disabled = true
+    } else{
+        qty++;
+        qtyLess.disabled = false
+        qty_box.innerText = `Qty : ${qty}`
+        price.innerText = `₹ ${qty * pro_price}` 
+        priceIn(pro_price)
+        if (dis_inp.value == "BB20") {
+            discount.innerText = `- ${Math.ceil(total * 20 / 100)}`
+            total_price.innerText = `₹ ${total - Math.ceil(total * 20 / 100)}`
+        }
+    }
+}
+
+function decQty(price, pro_price, qty_box, qtyAdd, qtyLess) {
+    if (qty == 1) {
+        qtyLess.disabled = true
+    } else {
+        qty--;
+        qtyAdd.disabled = false
+        qty_box.innerText = `Qty : ${qty}`
+        price.innerText = `₹ ${qty * pro_price}` 
+        priceDe(pro_price)
+        if (dis_inp.value == "BB20") {
+            discount.innerText = `- ${Math.ceil(total * 20 / 100)}`
+            total_price.innerText = `₹ ${total - Math.ceil(total * 20 / 100)}`
+        }
+    }
+}
+
+async function deleteProduct(prod){
+    try{
+        await fetch(`https://rich-gray-dolphin-hat.cyclic.app/cart/delete/${prod._id}` , {
+            method : "DELETE",
+            headers : {
+                authorization : `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+    location.reload()
+    // cartData()
+}
+
+
+
+
+
+
+
+
+// let arr = []
+
+// function cart(data){
+//     data.forEach(async function(el){
+//         // console.log(el.productId);
+//        try{
+//            let res = await fetch(`http://localhost:8000/products/productid/${el.productId}`)
+
+//            let final_data = await res.json()
+//             arr.push(final_data)
+//         //    console.log(final_data);
+//         // let c_data = await final_data.json()
+//         // console.log(c_data)
+//         //    arr.push(JSON.stringify(final_data))
+//        }
+//        catch(err){
+//         console.log(err);
+//        }
+//         // .then((res) => res.json())
+//         // .then((res) => {
+//         //     // arrPush(res)
+//         // })
+//         // .catch((err) => console.log(err))
+//     })
+    
+// }
+
+// console.log(arr)
+
+// let array=JSON.parse(localStorage.getItem("NykaaCart"))||[]
+// console.log(array)
+// // step 1 is to make a get request so that we can fetch the data
+// let cart__icon = document.querySelector(".cart--icon")
+
+// cart__icon.addEventListener("click" , ShowShoppingBag)
+
+// let shoppingBag = document.querySelector(".Shopping-bag-box")
+// let chat_ty = document.querySelector(".chatbox")
+// let arrowto_UP = document.querySelector(".arrow") 
+// function ShowShoppingBag() {
+//     shoppingBag.style.display = "block"
+//     chat_ty.style.display = "none"
+//     arrowto_UP.style.display = "none"
+// }
+
+// let shoppingBackBtn = document.querySelector(".shoppingback-btn")
+
+// shoppingBackBtn.addEventListener("click" , HideShoppingBag)
+
+// function HideShoppingBag() {
+//     shoppingBag.style.display = "none"
+//     chat_ty.style.display = "block"
+//     arrowto_UP.style.display = "block"
+// }
+
+// // get data from server 
+
+// let cart_div = document.querySelector(".multiple_prod_box");
+// appender(array)
+
+
+// let decreaseQuantity=(i)=>{
+
+// }
+// let increaseQuantity=(i)=>{
+
+// }
+
+
+
+
+// let qty = document.createElement("h4");
+        // qty.innerText = "Qty"
+        // let qty_box = document.createElement("select")
+        // qty_box.onchange = () => {
+        //     priceIn(+(qty_box.value))
+        // }
+
+        //     let q1 = document.createElement("option")
+        //     q1.textContent = 1
+        //     q1.value = 0
+
+        //     let q2 = document.createElement("option")
+        //     q2.textContent = 2
+        //     q2.value = 1 * el.price
+
+        //     let q3 = document.createElement("option")
+        //     q3.textContent = 3
+        //     q3.value = 2 * el.price
+
+        //     let q4 = document.createElement("option")
+        //     q4.textContent = 4
+        //     q4.value = 3 * el.price
+
+        //     let q5 = document.createElement("option")
+        //     q5.textContent = 5
+        //     q5.value = 4 * el.price
---
+++ scripts/product.js
@@ -0,0 +1,461 @@
+var slideindexval = 1;
+let i=localStorage.getItem("index")||0
+SHOWSLIDESOFPRODUCTS(slideindexval);
+
+function PlusSlidePro(n) {
+  SHOWSLIDESOFPRODUCTS(slideindexval += n);
+}
+
+function CURRENTSLIDE(n) {
+  SHOWSLIDESOFPRODUCTS(slideindexval = n);
+}
+
+function SHOWSLIDESOFPRODUCTS(n) {
+  var i;
+  var slides = document.getElementsByClassName("MYSlides");
+  var dots = document.getElementsByClassName("indicator");
+  if (n > slides.length) {slideindexval = 1}    
+  if (n < 1) {slideindexval = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideindexval-1].style.display = "block";  
+  dots[slideindexval-1].className += " active";
+}
+
+
+//Sorting functionality
+
+let SortPopularityBox = document.querySelector(".SortPopularityBox")
+
+let DownArrows = document.querySelector(".DownArrows")
+let UpArrows = document.querySelector(".UpArrows")
+let OptionsForSortBy = document.querySelector(".OptionsForSortBy")
+
+SortPopularityBox.addEventListener("click", ShowOptionsForSortBy)
+//UpArrows.addEventListener("click" , HideOptionsForSortBy )
+let flag = true
+function ShowOptionsForSortBy() {
+    if(flag == true) {
+    OptionsForSortBy.style.display = "grid"
+    DownArrows.style.display = "none"
+    UpArrows.style.display = "block"
+    flag = false
+    }else {
+        OptionsForSortBy.style.display = "none"
+        DownArrows.style.display = "block"
+        UpArrows.style.display = "none"
+        flag = true
+    }
+}
+
+// function HideOptionsForSortBy() {
+//     OptionsForSortBy.style.display = "none"
+//     DownArrows.style.display = "block"
+//     UpArrows.style.display = "none"
+// }   
+
+let Categery = document.querySelector(".Categery")
+
+Categery.addEventListener("click" , ShowCategoryBoxes)
+
+let CATCUT = document.querySelector(".CATCUT")
+
+
+let CATDOWN =  document.querySelector(".CATDOWN")
+
+
+let Multiple = document.querySelector(".multiple")
+
+let CATMAKEUP = document.querySelector(".CATMAKEUP")
+CATMAKEUP.addEventListener("click" , ShowMakeupResul)
+
+let Makeup_resul = document.querySelector(".Makeup-resul")
+
+let catflag = true
+
+function ShowCategoryBoxes() {
+  if(catflag == true) {
+  
+  Multiple.style.display = "block"
+  CATCUT.style.display = "block"
+  CATDOWN.style.display = "none" 
+  catflag = false
+  } else {
+    Multiple.style.display = "none"
+    CATCUT.style.display = "none"
+    CATDOWN.style.display = "block"
+    catflag = true
+  }
+}
+
+
+
+let Makeup_resulflag = true
+
+
+function ShowMakeupResul() {
+  if(Makeup_resulflag) {
+  Makeup_resul.style.display = "grid"
+  Makeup_resulflag = false
+  }else {
+    Makeup_resul.style.display = "none"
+    Makeup_resulflag = true
+  }
+}
+
+
+let FPRICE = document.querySelector(".FPRICE")
+let PRICEMULTI = document.querySelector(".PRICEMULTI")
+let PriDOWN = document.querySelector(".PriDOWN")
+let PriCUT = document.querySelector(".PriCUT")
+
+FPRICE.addEventListener("click" , SHOWPriceFil)
+
+let FPRICEflag = true
+
+function SHOWPriceFil() {
+  if(FPRICEflag) {
+    PRICEMULTI.style.display = "grid"
+    PriDOWN.style.display = "none"
+    PriCUT.style.display = "block"
+    FPRICEflag = false
+  }else {
+
+    PRICEMULTI.style.display = "none"
+    PriDOWN.style.display = "block"
+    PriCUT.style.display = "none"
+    FPRICEflag = true
+
+  }
+
+}
+
+
+
+
+// Products container
+
+let Products_Container = document.querySelector(".NykaaCosProducts-Container")
+
+
+
+
+ 
+
+  // let data = [
+  //   {
+  //     image:"https://images-static.nykaa.com/uploads/b21dc0cc-ff53-44e6-988c-624c0cc95c18.jpg?tr=w-240,cm-pad_resize",
+  //     name:"Beauty Cream",
+  //     price:799,
+  //     rating:"4.2",
+  //     discount:"30%",
+  //     brand:"nykaa cosmetics"
+  //   },
+  //   {
+  //     image:"https://images-static.nykaa.com/uploads/b21dc0cc-ff53-44e6-988c-624c0cc95c18.jpg?tr=w-240,cm-pad_resize",
+  //     name:"Beauty Cream",
+  //     price:799,
+  //     rating:"4.2",
+  //     discount:"30%",
+  //     brand:"nykaa cosmetics"
+  //   },
+  //   {
+  //     image:"https://images-static.nykaa.com/uploads/b21dc0cc-ff53-44e6-988c-624c0cc95c18.jpg?tr=w-240,cm-pad_resize",
+  //     name:"Beauty Cream",
+  //     price:799,
+  //     rating:"4.2",
+  //     discount:"30%",
+  //     brand:"nykaa cosmetics"
+  //   },
+  //   {
+  //     image:"https://images-static.nykaa.com/uploads/b21dc0cc-ff53-44e6-988c-624c0cc95c18.jpg?tr=w-240,cm-pad_resize",
+  //     name:"Beauty Cream",
+  //     price:799,
+  //     rating:"4.2",
+  //     discount:"30%",
+  //     brand:"nykaa cosmetics"
+  //   },
+  //   {
+  //     image:"https://images-static.nykaa.com/uploads/b21dc0cc-ff53-44e6-988c-624c0cc95c18.jpg?tr=w-240,cm-pad_resize",
+  //     name:"Beauty Cream",
+  //     price:799,
+  //     rating:"4.2",
+  //     discount:"30%",
+  //     brand:"nykaa cosmetics"
+  //   },
+  //   {
+  //     image:"https://images-static.nykaa.com/uploads/b21dc0cc-ff53-44e6-988c-624c0cc95c18.jpg?tr=w-240,cm-pad_resize",
+  //     name:"Beauty Cream",
+  //     price:799,
+  //     rating:"4.2",
+  //     discount:"30%",
+  //     brand:"nykaa cosmetics"
+  //   },
+  //   {
+  //     image:"https://images-static.nykaa.com/uploads/b21dc0cc-ff53-44e6-988c-624c0cc95c18.jpg?tr=w-240,cm-pad_resize",
+  //     name:"Beauty Cream",
+  //     price:799,
+  //     rating:"4.2",
+  //     discount:"30%",
+  //     brand:"nykaa cosmetics"
+  //   },
+  //   {
+  //     image:"https://images-static.nykaa.com/uploads/b21dc0cc-ff53-44e6-988c-624c0cc95c18.jpg?tr=w-240,cm-pad_resize",
+  //     name:"Beauty Cream",
+  //     price:799,
+  //     rating:"4.2",
+  //     discount:"30%",
+  //     brand:"nykaa cosmetics"
+  //   },
+  //   {
+  //     image:"https://images-static.nykaa.com/uploads/b21dc0cc-ff53-44e6-988c-624c0cc95c18.jpg?tr=w-240,cm-pad_resize",
+  //     name:"Beauty Cream",
+  //     price:799,
+  //     rating:"4.2",
+  //     discount:"30%",
+  //     brand:"nykaa cosmetics"
+  //   }
+  // ]
+
+  getAllProducts();
+   async function getAllProducts(){
+      const res  = await fetch(`https://rich-gray-dolphin-hat.cyclic.app/products`)//("http://localhost:8000/products")
+      const data = await res.json();
+      // console.log(data.products)
+      AppendToProCont(data.products)
+   }
+
+
+
+
+// GETCosNykaaProducts()
+
+function AppendToProCont(data) {
+
+  Products_Container.innerHTML = null
+
+data.forEach((prod) => {
+
+  let maindiv = document.createElement("div")
+  maindiv.setAttribute("class" , "main__div")
+
+  let div = document.createElement("div")
+  div.setAttribute("class" , "prod_box")
+
+  // console.log(prod.name)
+
+  let image = document.createElement("img")
+  image.src = prod.image[0]
+
+  div.onclick = function() {
+    GoToProductDetail(prod)
+  }
+
+  let pname = document.createElement("p")
+  pname.setAttribute("class", "pname")
+  pname.textContent = prod.name.substring(0,50)
+
+  let mrp = document.createElement("p")
+  mrp.setAttribute("class" , "mrp-text")
+  mrp.textContent = "MRP : ₹"
+
+  mrp_price = document.createElement("p")
+  mrp_price.textContent = mrp.textContent + prod.price
+
+  let cat = document.createElement("p")
+  cat.innerText = "Category : "+prod.category;
+
+  let gender = document.createElement("p")
+  gender.innerText =  "Gender : "+prod.gender;
+
+
+
+
+  let rati = document.createElement("p")
+  rati.setAttribute("class" , "starating")
+  rati.textContent = "Rating : "+prod.rating
+
+  let btndiv = document.createElement("div")
+  btndiv.setAttribute("class" , "btn_div")
+
+  let wish_btn = document.createElement("button")
+  wish_btn.setAttribute("class" , "wish_btn")
+  wish_btn.innerText = "♡"
+
+  wish_btn.onclick = function() {
+    AddToWishStore(prod)
+  }
+
+  let cart_btn = document.createElement("button")
+  cart_btn.setAttribute("class" , "cart_btn")
+  cart_btn.innerText = "ADD TO BAG"
+  cart_btn.onclick = () => {
+    addToCart(prod)
+  }
+
+  // cart_btn.onclick = function() {
+  //   i++
+  //   let index=localStorage.getItem("index")||0
+  //   localStorage.setItem("index",JSON.stringify(i))
+  //   AddToCartStore(prod)
+  //   document.querySelector(".cart--icon").innerHTML=`<i style="font-size:24px" class="fa">&#xf290;</i>${index}`
+  // }
+
+  // if(prod.brand == "nykaa cosmetics") {
+
+  btndiv.append(wish_btn, cart_btn)
+
+  div.append(image,pname, mrp_price,cat,gender,rati) //
+  maindiv.append(div, btndiv)
+  Products_Container.append(maindiv)
+  //  }
+
+})
+
+}
+
+if(localStorage.getItem("NykaaWish") === null) {
+  localStorage.setItem("NykaaWish" , JSON.stringify([]))
+}
+
+if(localStorage.getItem("NykaaCart") === null) {
+  localStorage.setItem("NykaaCart" , JSON.stringify([]))
+}
+
+function AddToCartStore(prodct) {
+  let getCartStore = JSON.parse(localStorage.getItem("NykaaCart"))
+
+  getCartStore.push(prodct)
+
+  localStorage.setItem("NykaaCart" , JSON.stringify(getCartStore))
+
+}
+
+function AddToWishStore(prodWish) {
+  let getWishStore = JSON.parse(localStorage.getItem("NykaaWish"))
+
+  getWishStore.push(prodWish)
+
+  localStorage.setItem("NykaaWish" , JSON.stringify(getWishStore))
+
+}
+
+if(localStorage.getItem("ProductDetail") === null) {
+  localStorage.setItem("ProductDetail" , JSON.stringify([]))
+}
+
+function GoToProductDetail(proDet) {
+
+  let getProdDet = JSON.parse(localStorage.getItem("ProductDetail"))
+  getProdDet.push(proDet)
+  
+  localStorage.setItem("ProductDetail" , JSON.stringify(getProdDet))
+
+  setTimeout(() => {
+    window.location.href = "ProductDetail.html"
+  },2500)
+
+}
+
+let SORTPOPULARITY = document.querySelector(".SORTPOPULARITY")
+SORTPOPULARITY.addEventListener("click" , fnSORTPOPULARITY)
+
+
+let pop_texts = document.querySelector(".pop-texts")
+
+
+let SORTHIGHTOLOW = document.querySelector(".SORTHIGHTOLOW")
+SORTHIGHTOLOW.addEventListener("click" , fnSORTHIGHTOLOW)
+
+ async function fnSORTHIGHTOLOW() {
+
+
+  let res = await fetch(`http://localhost:5000/api/products`)
+
+  let data = await res.json()
+    
+    let arr = data.sort(function (a,b){
+    
+      return b.price - a.price
+    })
+   AppendToProCont(arr)
+   SORTPOPULARITY.style.backgroundColor = "#fff"
+   SORTHIGHTOLOW.style.backgroundColor = "#fc3a84"
+   SORTLOWTOHIGH.style.backgroundColor = "#fff"
+   pop_texts.textContent = "Price: High To Low"
+    
+}
+
+let SORTLOWTOHIGH = document.querySelector(".SORTLOWTOHIGH")
+SORTLOWTOHIGH.addEventListener("click" , fnSORTLOWTOHIGH)
+
+async function fnSORTLOWTOHIGH() {
+
+  let res = await fetch(`http://localhost:5000/api/products`)
+
+  let data = await res.json()
+    
+    let arr = data.sort(function (a,b){
+    
+      return a.price - b.price
+    })
+   AppendToProCont(arr)
+   SORTPOPULARITY.style.backgroundColor = "#fff"
+  SORTHIGHTOLOW.style.backgroundColor = "#fff"
+  SORTLOWTOHIGH.style.backgroundColor = "#fc3a84"
+   pop_texts.textContent = "Price: Low To High"
+}
+
+
+function fnSORTPOPULARITY() {
+  pop_texts.textContent = "Popularity"
+  SORTPOPULARITY.style.backgroundColor = "#fc3a84"
+  SORTHIGHTOLOW.style.backgroundColor = "#fff"
+  SORTLOWTOHIGH.style.backgroundColor = "#fff"
+  GETCosNykaaProducts()
+}
+
+async function addToCart(prod){
+  const productId = prod._id
+  // console.log(productId)
+  // const payload = {
+  //   _id : productId
+  // }
+
+  try{
+    let cart_data = await fetch(`https://rich-gray-dolphin-hat.cyclic.app/cart/productData/${productId}` , {
+      headers : {
+        "authorization": `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    let finalData = await cart_data.json()
+    const payload = {
+      productDetails : finalData
+    }
+    // console.log(payload)
+    try {
+      //"http://localhost:8000/cart/add"
+      await fetch(`https://rich-gray-dolphin-hat.cyclic.app/cart/add`, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-type": "application/json",
+          "authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+  catch(err){
+    console.log(err);
+  }
+
+}
